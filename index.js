e70be1d83bd79fc3ccb4587f0ec1aae8903a1c76
Trishit <trishit10@gmail.com>
2022-09-21 23:45:32 +0530

    Javascript commit

---
+++ index.js
@@ -0,0 +1,45 @@
+// var a=50;
+// var b=70;
+// console.log(a+b);
+//  var arr=[19,11,14,13,18,12,8,9,5,3,7];
+// console.log(arr);
+// for(i=0;i<arr.length;i++){
+//     if(i==3){
+//         break;
+//     }
+//      console.log(arr[i]);
+// }
+// arr.push('Trishit');
+//console.log(arr);
+// arr.pop();
+// arr.unshift('me');
+// arr.sort();
+// arr.splice(2,6,4,"5,5,6");
+//  console.log(arr);
+// 
+// let eto="i am a good  footballer footballer";
+// // (console.log(eto.lastIndexOf('i'));
+// // console.log(eto.replace("footballer","basketball"));
+// e=eto.replace("i","Trishit");
+// console.log(e,eto);
+// let somoy=new Date();
+// console.log(somoy.getDay());
+    // let tar=document.getElementById('name');
+    // console.log(tar);
+//  let tar=document.getElementsByClassName('element');
+// console.log(tar[1]);
+//  tar=document.getElementsByTagName('div');
+// // console.log(tar);
+//  createdElement=document.createElement('p');
+//  createdElement.innerText="META";
+//  tar[0].appendChild(createdElement); 
+// createdElement2=document.createElement('b');
+//  createdElement2.innerText="Whats App";
+//  tar[0].replaceChild(createdElement2 , createdElement);
+//   function clicked(){
+//     console.log('I am pressed');
+//   }
+// cry.addEventListener('Mousedown' , function(){
+//       // document.querySelectorAll('.element')[1].innerHTML=<b>" INSTAGRAM "</b>
+//     console.log("i have clicked");
+// });
